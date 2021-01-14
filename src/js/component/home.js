7f90d5bce1cd4e89b4c10f775cd384c07b4ab9bf
hexbreak <theownerproject@gmail.com>
2021-01-14 01:24:02 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	const [theList, setList] = useState([
+	const [theList, getList] = useState([
 		"clean the house",
 		"go shopping",
 		"wash the car"
 	]);
-
+	// on useState, if you change whats inside the square brackets, it will  fill the input field to whatever is inside
+	// the input field is now in control of the userinput
 	const [userInput, setUserInput] = useState([""]);
+
+	const handleKeyUp = event => {
+		if (event.keyCode == 13) {
+			getList(theList.concat(userInput));
+		}
+	};
 
 	return (
 		<div className="text-center mt-5">
 			<div className="todoList">
 				<input
-					className="todoInput" // event can be named anything since the arrow function knows what to do
+					className="todoInput"
+					// event can be named anything since the arrow function knows what to do
 					onChange={event => setUserInput(event.target.value)}
+					// event.target.value and value have no relation to each other.
+					value={userInput}
+					onkeyUp={handleKeyUp}
 				/>
 				<button className="addButton">click to add!</button>
 				<ul className="list-group">
