e0e75e41b7b86cc1b5dbb121b7110cf496168d95
hexbreak <theownerproject@gmail.com>
2021-01-15 04:40:47 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -29,35 +29,37 @@ export function Home() {
 	};
 
 	return (
-		<div className="d-flex mx-auto justify-content-center">
-			<h1 className="row">TODOs</h1>
-			<div className="card">
-				<input
-					className="todoInput"
-					// event can be named anything since the arrow function knows what to do
-					onChange={event => setUserInput(event.target.value)}
-					// event.target.value and value have no relation to each other.
-					value={userInput}
-					onKeyUp={handleKeyUp}
-				/>
-				<ul className="list-group list-group-flush">
-					{theList.map((value, index) => {
-						return (
-							<li className="list-group-item" key={index}>
-								{value}
-								<button
-									type="button"
-									// onClick is an attribute, the function code runs if its {itemDelete}, so you must
-									// run onClick with an arrow function in order to run when the user clicks
-									// onClick runs as soon as the page loads if it's not on an arrow function
-									onClick={() => itemDelete(index)}
-									className="btn btn-primary">
-									X
-								</button>
-							</li>
-						);
-					})}
-				</ul>
+		<div className="container">
+			<h1 className="text-center">ToDo List</h1>
+			<div className="d-flex mx-auto justify-content-center">
+				<div className="card">
+					<input
+						className="todoInput"
+						// event can be named anything since the arrow function knows what to do
+						onChange={event => setUserInput(event.target.value)}
+						// event.target.value and value have no relation to each other.
+						value={userInput}
+						onKeyUp={handleKeyUp}
+					/>
+					<ul className="list-group list-group-flush">
+						{theList.map((value, index) => {
+							return (
+								<li className="list-group-item" key={index}>
+									{value}
+									<button
+										type="button"
+										// onClick is an attribute, the function code runs if its {itemDelete}, so you must
+										// run onClick with an arrow function in order to run when the user clicks
+										// onClick runs as soon as the page loads if it's not on an arrow function
+										onClick={() => itemDelete(index)}
+										className="btn btn-primary">
+										X
+									</button>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
